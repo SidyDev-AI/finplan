980889f0c23e289ee0b2c5641e3a85ae88736817
Borges Costta <sidydev.io@gmail.com>
2025-05-07 19:42:15 -0300

    Feature RF003 recuperação de senha

--- src/pages/alterarSenha.php
+++ src/pages/alterarSenha.php
@@ -1,14 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario_recuperacao'])) {
+    header('Location: confirmarEmail.php');
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="/src/css/index.css">
+  <link rel="stylesheet" href="../css/alterarSenha.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <title>FinPlan | Login</title>
+  <title>FinPlan | Alterar Senha</title>
 </head>
 <body>
   <section id="container">
@@ -22,30 +30,22 @@
     </div>
     <div id="content">
       <img class="logo" src="/src/img/logo.png" alt="Logo FinPlan">
-      <h1>Acesse sua conta</h1>
-      <form id="formLogin" action="src/backend/login.php" method="POST">
-        <div class="email">
-          <i class="fa-solid fa-envelope"></i>
-          <input type="email" id="email" name="email" placeholder="E-mail" required>
+      <p>Please enter your new password</p>
+      <form id="formLogin" action="../backend/alterarSenha.php" method="POST">
+        <div class="password">
+          <i class="fa-solid fa-lock"></i>
+          <input type="password" id="senha" name="senha" placeholder="Nova Senha" required minlength="6">
         </div>
         <div class="password">
           <i class="fa-solid fa-lock"></i>
-          <input type="password" id="senha" name="senha" placeholder="Senha" required minlength="6">
+          <input type="password" id="confirmar_senha" name="confirmar_senha" placeholder="Confirmar Senha" required minlength="6">
         </div>
-        <div class="conectado">
-          <input type="checkbox" name="" id="">
-          <p>Permanercer conectado</p>
-        </div>
-        <button type="submit" class="form-btn">Login</button>
+        <button type="submit" class="form-btn">Confirmar</button>
       </form>
-      <p><a class="esqueci" href="/src/pages/confirmarEmail.php">Esqueci minha senha!</a></p>
-      <a href="/src/pages/register.html">Criar Nova Conta</a>
-      <p> --------------- OU --------------- </p>
-      <div class="social-media">
-        <a href="#"><img src="/src/img/icons/google.png" alt="Link google account login"></a>
-        <a href="#"><img src="/src/img/icons/apple.png" alt="Link apple account login"></a>
-      </div>
     </div>
+    <?php if(isset($_GET['erro'])): ?>
+        <p style="color:red;">As senhas não coincidem!</p>
+    <?php endif; ?>
   </section>
 </body>
 </html>
